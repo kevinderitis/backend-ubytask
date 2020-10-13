3a80d83dc512aed2fc882d6075fe218b204bc7f5
kevinderitis <32942115+kevinderitis@users.noreply.github.com>
2020-10-13 16:28:39 -0300

    commit

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -7,25 +7,33 @@ router.post('/login', async (req, res) => {
     const usuario = await user.findAll({
         where: { "nombre": req.body.nombre }
     });
-    jwt.sign({ usuario }, 'secretkey', (err, token) => {
-        res.json({
-            token
+
+    if (usuario == "") {
+        res.sendStatus(403);
+    } else {
+
+        jwt.sign({ usuario }, 'secretkey', (err, token) => {
+            res.json({
+                token
+            });
         });
-    });
+    }
+
 
 });
 
 router.get('/', validarToken, (req, res) => {
     jwt.verify(req.token, 'secretkey', (err, authData) => {
         if (err) {
-res.sendStatus(403);
+            res.sendStatus(403);
         } else {
-            res.json({ message: 'Token validado',
-            authData 
-        });
+            res.json({
+                message: 'Token validado',
+                authData
+            });
         }
     });
-    
+
 });
 
 function validarToken(req, res, next) {
