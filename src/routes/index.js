e9bec22401621249064c794ad985aff179aca535
kevinderitis <32942115+kevinderitis@users.noreply.github.com>
2020-10-24 19:11:09 -0300

    commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const {Router} = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.json({"Titulo": "Elke"});
+    res.json({"Bienvenida": "Bienvenidos a Ubytask"});
     });
 
 
